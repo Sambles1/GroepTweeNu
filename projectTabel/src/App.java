6a4741b3bb94fa948cf610ca9109b3b50dc606a1
andre7455 <42913113+andre7455@users.noreply.github.com>
2020-03-31 13:11:04 +0200

    anus is niet cool

--- projectTabel/src/App.java
+++ projectTabel/src/App.java
@@ -1,7 +1,8 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("hello world");
-
+        System.out.println("aids");
+        System.out.println("anus");
     }
 }
 
