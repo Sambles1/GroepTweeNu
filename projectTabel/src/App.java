707733cac300141e0f8fd8ce4297252347cb170c
andre7455 <42913113+andre7455@users.noreply.github.com>
2020-03-31 13:17:50 +0200

    nee

--- projectTabel/src/App.java
+++ projectTabel/src/App.java
@@ -1,8 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("let op hoor!");
-        System.out.println("nee");
+
     }
 }
 
