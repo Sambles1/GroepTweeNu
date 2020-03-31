f56a2d853d30a90bccf94eb8a1ffdb806fcd25e8
andre7455 <42913113+andre7455@users.noreply.github.com>
2020-03-31 13:30:18 +0200

    is ze ook niet

--- projectTabel/src/App.java
+++ projectTabel/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("anus");
-        if(1 == 1){
+        if(1 == 3){
             System.out.println("je oma dik");
         }
     }
