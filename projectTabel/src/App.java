31c76ec7ba4d895626d797b0c1254a400e545627
Sambles1 <62054945+Sambles1@users.noreply.github.com>
2020-03-31 13:28:13 +0200

    ja hoor3

--- projectTabel/src/App.java
+++ projectTabel/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("anus");
+        if(1 == 1){
+            System.out.println("je oma dik");
+        }
     }
 }
