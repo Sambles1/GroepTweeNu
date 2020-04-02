795f1e367f3db05a4df99a002bf7524a61c6668c
Sambles1 <62054945+Sambles1@users.noreply.github.com>
2020-04-02 15:13:08 +0200

    poging 3

--- Table/src/App.java
+++ Table/src/App.java
@@ -1,2 +1,9 @@
 public class App {
+    public static void main(String[] args) {
+        for(int i = 0; i < 100000; i++){
+            double randomizedRaw = 1 + Math.random() * 10000;
+            int randomized = (int) randomizedRaw;
+            System.out.println(randomized);
+        }
+    }
 }
