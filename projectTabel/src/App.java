1a66fc793dc03481093cbcb80affda1ec495aae4
Sambles1 <62054945+Sambles1@users.noreply.github.com>
2020-04-02 13:47:21 +0200

    ff leeg

--- projectTabel/src/App.java
+++ projectTabel/src/App.java
@@ -1,8 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("anus");
-        if(1 == 1){
-            System.out.println("je oma dik");
-        }
+
     }
 }
