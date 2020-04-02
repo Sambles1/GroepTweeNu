7e16308be2bce0e71147356735aeb36ba3d6e80c
andre7455 <42913113+andre7455@users.noreply.github.com>
2020-04-02 15:51:22 +0200

    e

--- Table/src/App.java
+++ Table/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) {
         Randomgetal getal = new Randomgetal();
         int[] array = new int[100000];
-        Print print = new Print(array[];);
+        Print print = new Print();
+        print.print(array);
     }
 }
