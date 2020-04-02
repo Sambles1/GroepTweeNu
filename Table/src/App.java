934f7c37c773dda561677b6271c82647da313fb4
andre7455 <42913113+andre7455@users.noreply.github.com>
2020-04-02 15:16:24 +0200

    ik heb het aantal gecapped op 10000 inplaats van dat het 80000 kon zijn

--- Table/src/App.java
+++ Table/src/App.java
@@ -1,9 +1,14 @@
 public class App {
     public static void main(String[] args) {
-        for(int i = 0; i < 100000; i++){
+        int totaal = 100000;
+        for(int i = 0; i < totaal; i++){
             double randomizedRaw = 1 + Math.random() * 10000;
             int randomized = (int) randomizedRaw;
-            System.out.println(randomized);
+            if (randomized > 10000){
+                totaal++;
+            } else {
+                System.out.println(randomized);
+            }
         }
     }
 }
