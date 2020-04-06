6f585bc577b7c4d4e9d602b6a679f33e2742f78c
andre7455 <42913113+andre7455@users.noreply.github.com>
2020-04-06 15:16:17 +0200

    hopenlijk werkt dit wel

--- Table/src/Print.java
+++ Table/src/Print.java
@@ -2,7 +2,6 @@ public class Print {
     public void print(int[] array) {
         int x = array.length / 10;
         int y = 0;
-        System.out.println();
         for (int i = 0; i < x; i++) {
             System.out.print(array[y]);
             y++;
