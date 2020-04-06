e9c517d5ef80c335bfc695b0fc32f3c4259e7fa3
andre7455 <42913113+andre7455@users.noreply.github.com>
2020-04-06 23:36:00 +0200

    test om out of bounts te checken

--- Table/src/Print.java
+++ Table/src/Print.java
@@ -1,29 +1,31 @@
 public class Print {
     public void print(int[] array) {
         int x = array.length / 10;
+        x--;
         int y = 0;
         for (int i = 0; i < x; i++) {
-            System.out.print(array[y]);
-            y++;
-            System.out.print(" " + array[y]);
-            y++;
-            System.out.print(" " + array[y]);
-            y++;
-            System.out.print(" " + array[y]);
-            y++;
-            System.out.print(" " + array[y]);
-            y++;
-            System.out.print(" " + array[y]);
-            y++;
-            System.out.print(" " + array[y]);
-            y++;
-            System.out.print(" " + array[y]);
-            y++;
-            System.out.print(" " + array[y]);
-            y++;
-            System.out.print(" " + array[y]);
-            y++;
-            System.out.println(" ");
+            //System.out.print(array[y]);
+            //y++;
+            //System.out.print(" " + array[y]);
+            //y++;
+            //System.out.print(" " + array[y]);
+            //y++;
+            //System.out.print(" " + array[y]);
+            //y++;
+            //System.out.print(" " + array[y]);
+            //y++;
+            //System.out.print(" " + array[y]);
+            //y++;
+            //System.out.print(" " + array[y]);
+            //y++;
+            //System.out.print(" " + array[y]);
+            //y++;
+            //System.out.print(" " + array[y]);
+            //y++;
+            //System.out.print(" " + array[y]);
+            //y++;
+            //System.out.println("");
+            System.out.println(x);
         }
     }
 }
