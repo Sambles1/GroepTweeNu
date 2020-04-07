26bb17f3897425af77a8d2c65cab8061b2eb26d4
andre7455 <42913113+andre7455@users.noreply.github.com>
2020-04-07 22:30:25 +0200

    ik heb de shit bijgewerkt dat er geen out of bounds meer is

--- Table/src/Print.java
+++ Table/src/Print.java
@@ -4,27 +4,27 @@ public class Print {
         x--;
         int y = 0;
         for (int i = 0; i < x; i++) {
-            //System.out.print(array[y]);
-            //y++;
-            //System.out.print(" " + array[y]);
-            //y++;
-            //System.out.print(" " + array[y]);
-            //y++;
-            //System.out.print(" " + array[y]);
-            //y++;
-            //System.out.print(" " + array[y]);
-            //y++;
-            //System.out.print(" " + array[y]);
-            //y++;
-            //System.out.print(" " + array[y]);
-            //y++;
-            //System.out.print(" " + array[y]);
-            //y++;
-            //System.out.print(" " + array[y]);
-            //y++;
-            //System.out.print(" " + array[y]);
-            //y++;
-            //System.out.println("");
+            System.out.print(array[y]);
+            y++;
+            System.out.print(" " + array[y]);
+            y++;
+            System.out.print(" " + array[y]);
+            y++;
+            System.out.print(" " + array[y]);
+            y++;
+            System.out.print(" " + array[y]);
+            y++;
+            System.out.print(" " + array[y]);
+            y++;
+            System.out.print(" " + array[y]);
+            y++;
+            System.out.print(" " + array[y]);
+            y++;
+            System.out.print(" " + array[y]);
+            y++;
+            System.out.print(" " + array[y]);
+            y++;
+            System.out.println("");
             System.out.println(x);
         }
     }
