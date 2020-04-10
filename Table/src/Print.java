3aa8d84c850b22eed5518fb59e5672c0929fb0a4
andre7455 <42913113+andre7455@users.noreply.github.com>
2020-04-10 11:32:28 +0200

    de laatste 10 getallen werden niet uigeprint dat kwam door een oplossing die ik had geprobeerd om de out of bounds exeption op te lossen maar dat was niet meer nodig

--- Table/src/Print.java
+++ Table/src/Print.java
@@ -1,7 +1,6 @@
 public class Print {
     public void print(int[] array) {
         int x = array.length / 10;
-        x--;
         int y = 0;
         for (int i = 0; i < x; i++) {
             System.out.print(array[y]);
