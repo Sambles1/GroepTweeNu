88c373f512299b4c864856be1113148260a3dbe7
andre7455 <42913113+andre7455@users.noreply.github.com>
2020-04-15 13:01:41 +0200

    dit moet werken van de kaas koning in( wat test/schoonmaak werk)

--- Table/src/App.java
+++ Table/src/App.java
@@ -6,7 +6,8 @@ public class App {
         Randomgetal getal = new Randomgetal();
         int[] array = getal.getArray();
         Print print = new Print();
-        print.print(array);
+        //print.print(array);
+
         System.out.println("Which sorting would you like to use?");
         System.out.println("[ Quicksort / Bogosort / Bubblesort ]");
         Scanner input = new Scanner(System.in);
@@ -15,36 +16,38 @@ public class App {
         app.sort(array); //code met meneer
 
 
-        if (answer.equals("Quicksort")) {
+        // if (answer.equals("Quicksort")) {
 
-            System.out.println("Quicksort done!");
-        } else if (answer.equals("Bogosort")) {
-            //bogosort goes here
-//           System.out.println("Bogosort done!");
-        } else if (answer.equals("Bubblesort")) {
-            //bubblesort goes here
-//           System.out.println("Bubblesort done!");
-        } else {
-            System.out.println("input not valid!");
-        }
+        //     System.out.println("Quicksort done!");
+        // } else if (answer.equals("Bogosort")) {
+        //     //bogosort goes here
+////      System.out.println("Bogosort done!");
+        // } else if (answer.equals("Bubblesort")) {
+        //     //bubblesort goes here
+////      System.out.println("Bubblesort done!");
+        // } else {
+        //     System.out.println("input not valid!");
+        // }
+        //
+
+
     }
 
-    public int[] sort(int[] array){
-        for (int i = 0; i < array.length; i++){
-            for (int j = i; j < array.length; j++){
-                if (array[i] < array[j]){
+    public int[] sort(int[] array2) {
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = i; j < array2.length; j++) {
+                if (array2[i] < array2[j]) {
                     int temp;
-                    temp = array[j];
-                    array[j] = array[i];
-                    array[i] = temp;
-                }
-                else {
+                    temp = array2[j];
+                    array2[j] = array2[i];
+                    array2[i] = temp;
+                } else {
                     break;
                 }
             }
         }
-        return array;
+
+        return array2;
     }
 }
-
 
