e878e6789f6529afa8b97645a306a602c22c1d56
andre7455 <42913113+andre7455@users.noreply.github.com>
2020-04-20 13:38:16 +0200

    met 3 getallen

--- Table/src/Randomgetal.java
+++ Table/src/Randomgetal.java
@@ -1,6 +1,6 @@
 public class Randomgetal {
     public int[] getArray(){
-        int totaal = 3;  //het aantal getallen wat word toevegoegt aan het array en ook de groote van array bepaald
+        int totaal = 15;  //het aantal getallen wat word toevegoegt aan het array en ook de groote van array bepaald
         int[] getal = new int[totaal];
         for (int i = 0; i < totaal; i++) {
             double randomizedRaw = 1 + Math.random() * 10000;
